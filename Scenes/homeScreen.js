ff77091ea895805c2fa113fcd4d08ae495ba59b9
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-04-17 22:08:30 -0700

    added menu music

--- Scenes/homeScreen.js
+++ Scenes/homeScreen.js
@@ -7,6 +7,10 @@ class homeScreen extends Phaser.Scene {
         this.load.path = 'assets/';
         this.load.image('background', 'background.png')
         this.load.image('title', 'title.png')
+        this.load.image('play', 'play.png')
+        this.load.image('options', 'options.png')
+        this.load.image('quit', 'quit.png')
+        this.load.audio('menuTheme', 'MTV Music Generator.mp3')
     }
 
     create () {
@@ -15,6 +19,18 @@ class homeScreen extends Phaser.Scene {
 
         let titleImage = this.add.image(400, 150, 'title');
         titleImage.setScale(.2);
+
+        let playImage = this.add.image(400, 300, 'play');
+        playImage.setScale(.25);
+
+        let optionImage = this.add.image(400, 375, 'options');
+        optionImage.setScale(.25);
+
+        let quitImage = this.add.image(400, 450, 'quit');
+        quitImage.setScale(.25);
+
+        let menuTheme = this.sound.add('menuTheme');
+        menuTheme.play();
 
 
     }
