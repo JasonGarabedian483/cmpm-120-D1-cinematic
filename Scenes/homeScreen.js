60463a7005eed9709c1916fd043f4b1b73d6d577
JasonGarabedian483 <jrgarabe@ucsc.edu>
2026-04-25 18:53:57 -0700

    changed charImage delay

--- Scenes/homeScreen.js
+++ Scenes/homeScreen.js
@@ -67,7 +67,7 @@ class homeScreen extends Phaser.Scene {
 
         this.tweens.add({
             targets: charImage,
-            delay: 2000,
+            delay: 2750,
             angle: 360,
             duration: 750,
             repeat: 2
